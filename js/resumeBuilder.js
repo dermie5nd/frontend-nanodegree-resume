264b2ae9dd74c0ba655f1dfe3840782ab215bffc
dermie5nd <mtmcdermott5@gmail.com>
2016-02-19 17:43:21 -0500

    Removed code
    removed uneeded hello world

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append([Michael McDermott]);
+var name = "Michael McDermott";
+var role = "Lead Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
